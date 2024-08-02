28a850fde9875e2bd77fda59ead443db5c081624
Lamine Kouissi <laminekouissi@gmail.com>
2024-08-02 19:56:44 +0100

    add AuthFromCtx() for the next filters to retrieve the authenticated user and prevent any context Keys collusion in the future

--- filters/auth.go
+++ filters/auth.go
@@ -1,11 +1,23 @@
 package filters
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+)
 
 type Auth struct {
 	nextFilter Filter
 }
+type contextKey int
 
+const (
+	authKey contextKey = iota
+)
+
+func AuthFromCtx(ctx context.Context) (string, bool) {
+	auth, ok := ctx.Value(authKey).(string)
+	return auth, ok
+}
 func (au *Auth) Process(req *http.Request, res *http.Response) error {
 	return nil
 }
